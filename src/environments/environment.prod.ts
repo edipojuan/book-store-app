1923d3727a6f5378535e8af7937c107e1eca3fb8
edipojuan <edipojs@gmail.com>
2019-09-06 19:29:15 -0400

    Updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:5000/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBMnQa9W_4aKrlSQ4kIlm_JQkAfNJJUZPM',
+    authDomain: 'book-store-d9ae0.firebaseapp.com',
+    storageBucket: 'book-store-d9ae0.appspot.com',
+    projectId: 'book-store-d9ae0'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   production: false,
+  baseUrl: 'http://localhost:5000/',
   firebaseConfig: {
-    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    databaseURL: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    projectId: 'xxxxxxxxxxxxx',
-    storageBucket: 'xxxxxxxxxxxxxxx',
-    messagingSenderId: 'xxxxxxxxxxxxxx',
-    appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    apiKey: 'AIzaSyBMnQa9W_4aKrlSQ4kIlm_JQkAfNJJUZPM',
+    authDomain: 'book-store-d9ae0.firebaseapp.com',
+    storageBucket: 'book-store-d9ae0.appspot.com',
+    projectId: 'book-store-d9ae0'
   }
 };
